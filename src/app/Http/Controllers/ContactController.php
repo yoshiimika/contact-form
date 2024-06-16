9aa61a360b77c269d65138ebb5e596ce74729b7e
yoshii mika <mika.1120.radwimps@icloud.com>
2024-06-17 05:31:11 +0900

    リモートリポジトリの変更

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -7,17 +7,22 @@ use App\Models\Contact;
 
 class ContactController extends Controller
 {
+    //フォーム入力ページの表示
     public function index()
     {
         return view('index');
     }
 
+    //入力内容確認ページの表示
+    //CRUD処理におけるRead（取得）
     public function confirm(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         return view('confirm', compact('contact'));
     }
 
+    //データの保存処理とお問い合わせ完了ページの表示
+    //CRUD処理におけるCreate（追加）
     public function store(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
